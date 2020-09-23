25585935ec7c6ee38efb2be444335b16fa1988ad
Travis Lee <travisslee223@gmail.com>
2020-09-23 17:30:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,38 @@
 class Owner
   # code goes here
+  attr_accessor  :pets
+  
+  attr_reader :name, :species
+  @@count_owners = 0
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+  
+  def cats 
+    @pets[:cats].each do |cat|
+      cat
+    end
+  end
+  
+  
 end
